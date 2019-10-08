e3c73bba695de2666a1d0b0e580ede2c197378cc
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-10-08 21:26:13 +0300

    up

--- [5]20.09-MultidimensionalArrays[LAB]/6. Jagged-Array Modification/Program.cs
+++ [5]20.09-MultidimensionalArrays[LAB]/6. Jagged-Array Modification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _6._Jagged_Array_Modification
 {
@@ -6,7 +7,63 @@ namespace _6._Jagged_Array_Modification
     {
         static void Main(string[] args)
         {
-             
+            int n = int.Parse(Console.ReadLine());
+
+            var arr = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    arr[row, col] = numbers[col];
+                }
+            }
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var commands = input.Split();
+                var operation = commands[0];
+                int row = int.Parse(commands[1]);
+                int col = int.Parse(commands[2]);
+                int value = int.Parse(commands[3]);
+
+                if (operation == "Add")
+                {
+                    if (row < arr.GetLength(0) && col < arr.GetLength(1) && row >= 0 && col >= 0)
+                    {
+                        arr[row, col] += value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+                else if (operation == "Subtract")
+                {
+                    if (row < arr.GetLength(0) && col < arr.GetLength(1) && row >= 0 && col >= 0)
+                    {
+                        arr[row, col] -= value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
