94fb4d81c7ffcc1f4f84417b8f78bcc59fb498f3
Ivaylo Iliev <alfara@students.softuni.bg>
2019-10-10 20:15:39 +0300

    sets up

---
+++ [8]30.09-SetsAndDictionaries[EXERCISE]/02. Sets of Elements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setsNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = setsNumber[0];
+            int m = setsNumber[1];
+
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSet.Add(number);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondSet.Add(number);
+            }
+
+            firstSet.IntersectWith(secondSet);
+
+            Console.WriteLine(String.Join(" ", firstSet));
+        }
+    }
+}
---
+++ [8]30.09-SetsAndDictionaries[EXERCISE]/03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var chemicalElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+
+                foreach (var element in input)
+                {
+                    chemicalElements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicalElements));
+        }
+    }
+}
