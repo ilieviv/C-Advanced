772a51e409b128ca871ee6a691f9f3a8d3dc2088
Ivaylo Iliev <alfara@students.softuni.bg>
2019-10-17 22:41:09 +0300

    classes exercise update

---
+++ 10.10-DefiningClasses[EXERCISE]/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Name = "Pesho";
+            person1.Age = 20;
+
+            Person person2 = new Person
+            {
+                Name = "Gosho",
+                Age = 18
+            };
+
+            Person person3 = new Person
+            {
+                Name = "Stamat"
+            };
+            person3.Age = 43;
+        }
+    }
+}
---
+++ 10.10-DefiningClasses[EXERCISE]/03. Oldest Family Member/Family.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            this.familyMembers = new List<Person>();
+        }
+        public void AddMember(Person familyMember)
+        {
+            this.familyMembers.Add(familyMember);
+        }
+
+        public Person GetOldestMember()
+        {
+            var person = familyMembers.OrderByDescending(p => p.Age).FirstOrDefault();
+            return person;
+        }
+    }
+}
