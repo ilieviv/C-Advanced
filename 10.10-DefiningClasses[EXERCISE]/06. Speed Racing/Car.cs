db4932b09d72ff958c0ae415f33af9a017093490
Ivaylo Iliev <alfara@students.softuni.bg>
2019-10-21 14:06:51 +0300

    classes exercise update

---
+++ 10.10-DefiningClasses[EXERCISE]/06. Speed Racing/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._Speed_Racing
+{
+    public class Car
+    {
+        public string model;
+        public double fuelAmount;
+        public double fuelConsumptionPerKilometer;
+        public double traveledDistance;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TraveledDistance = 0;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+
+        public double TraveledDistance
+        {
+            get { return traveledDistance; }
+            set { traveledDistance = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.fuelAmount < distance * fuelConsumptionPerKilometer)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                this.fuelAmount -= distance * this.fuelConsumptionPerKilometer;
+                this.traveledDistance += distance;
+            }
+        }
+    }
+}
---
+++ 10.10-DefiningClasses[EXERCISE]/06. Speed Racing/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Speed_Racing
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            Car[] cars = new Car[numberOfCars];
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var currentCar = Console.ReadLine().Split();
+
+                string model = currentCar[0];
+                double fuelAmount = double.Parse(currentCar[1]);
+                double fuelCost = double.Parse(currentCar[2]);
+
+                cars[i] = new Car(model, fuelAmount, fuelCost);
+            }
+
+            var input = Console.ReadLine();
+
+            while (true)
+            {
+                if (input == "End")
+                {
+                    break;
+                }
+
+                var splittedInput = input.Split();
+
+                string model = splittedInput[1];
+                double distance = double.Parse(splittedInput[2]);
+
+                cars.Where(c => c.model == model).ToList().ForEach(c => c.Drive(distance));
+
+                input = Console.ReadLine();
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine("{0} {1:F2} {2}", car.model, car.fuelAmount, car.traveledDistance);
+            }
+        }
+    }
+}
