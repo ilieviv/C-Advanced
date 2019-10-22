5cf5095a5dad94f1f0c1c08d55ae978641067ec1
Ivaylo Iliev <alfara@students.softuni.bg>
2019-10-22 19:52:16 +0300

    sets update

---
+++ [8]30.09-SetsAndDictionaries[EXERCISE]/08. Ranking/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var contests = new Dictionary<string, string>();
+
+            while (input != "end of contests")
+            {
+                var splittedInput = input.Split(":");
+
+                string contest = splittedInput[0];
+                string password = splittedInput[1];
+
+                contests.Add(contest, password);
+                input = Console.ReadLine();
+            }
+
+            string inputSubmissions = Console.ReadLine();
+
+            var participants = new Dictionary<string, Dictionary<string, int>>();
+
+            while (inputSubmissions != "end of submissions")
+            {
+                var splittedInput = inputSubmissions.Split("=>");
+
+                string contest = splittedInput[0];
+                string password = splittedInput[1];
+                string username = splittedInput[2];
+                int points = int.Parse(splittedInput[3]);
+
+                if (contests.ContainsKey(contest))
+                {
+                    if (contests[contest] == password)
+                    {
+                        if (!participants.ContainsKey(username))
+                        {
+                            participants.Add(username, new Dictionary<string, int>());
+                            participants[username].Add(contest, points);
+                        }
+                        else
+                        {
+                            if (!participants[username].ContainsKey(contest) && contests[contest] == password)
+                            {
+                                participants[username].Add(contest, points);
+                            }
+                            else
+                            {
+                                if (participants[username][contest] < points && contests[contest] == password)
+                                {
+                                    participants[username][contest] = points;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                inputSubmissions = Console.ReadLine();
+            }
+
+            int bestScore = 0;
+            string bestUser = string.Empty;
+
+            foreach (var person in participants)
+            {
+
+                int sum = 0;
+                foreach (var exam in person.Value)
+                {
+                    sum += exam.Value;
+                }
+
+                if (sum > bestScore)
+                {
+                    bestScore = sum;
+                    bestUser = person.Key;
+                }
+            }
+
+            Console.WriteLine($"Best candidate is {bestUser} with total {bestScore} points.");
+            Console.WriteLine("Ranking:");
+            foreach (var participant in participants.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{participant.Key}");
+
+                foreach (var exam in participant.Value.OrderBy(x => x.Key))
+                {
+                    Console.WriteLine($"#  {exam.Key} -> {exam.Value}");
+                }
+            }
+        }
+    }
+}
