92d326cf61d8b890809b99b59a318b7f3f08eef1
Ivaylo Iliev <alfara@students.softuni.bg>
2019-10-22 13:54:36 +0300

    multidimensional arrays update

---
+++ [6]26.09-MultidimensionalArrays{EXERCISE]/8. Bombs/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Bombs
+{
+    class Program
+    {
+        static int matrixRow;
+        static int matrixCol;
+        static int[,] matrix;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            matrix = new int[n, n];
+
+            PopulateMatrix();
+
+            var coordinates = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                var currentCordinates = coordinates[i].Split(',').Select(int.Parse).ToArray();
+                matrixRow = currentCordinates[0];
+                matrixCol = currentCordinates[1];
+
+                int bomb = matrix[matrixRow, matrixCol];
+
+                if (matrix[matrixRow, matrixCol] == 0)
+                {
+                    bomb = 0;
+                }
+
+
+                if (IsInside(matrixRow - 1, matrixCol - 1) && (matrix[matrixRow - 1, matrixCol - 1] > 0))
+                {
+                    matrix[matrixRow - 1, matrixCol - 1] -= bomb;
+                }
+
+                if (IsInside(matrixRow - 1, matrixCol) && (matrix[matrixRow - 1, matrixCol] > 0))
+                {
+                    matrix[matrixRow - 1, matrixCol] -= bomb;
+                }
+                if (IsInside(matrixRow - 1, matrixCol + 1) && (matrix[matrixRow - 1, matrixCol + 1] > 0))
+                {
+                    matrix[matrixRow - 1, matrixCol + 1] -= bomb;
+                }
+
+                if (IsInside(matrixRow, matrixCol + 1) && (matrix[matrixRow, matrixCol + 1] > 0))
+                {
+                    matrix[matrixRow, matrixCol + 1] -= bomb;
+                }
+
+                if (IsInside(matrixRow + 1, matrixCol + 1) && (matrix[matrixRow + 1, matrixCol + 1] > 0))
+                {
+                    matrix[matrixRow + 1, matrixCol + 1] -= bomb;
+                }
+
+                if (IsInside(matrixRow + 1, matrixCol) && (matrix[matrixRow + 1, matrixCol] > 0))
+                {
+                    matrix[matrixRow + 1, matrixCol] -= bomb; //
+                }
+
+                if (IsInside(matrixRow + 1, matrixCol - 1) && (matrix[matrixRow + 1, matrixCol - 1] > 0))
+                {
+                    matrix[matrixRow + 1, matrixCol - 1] -= bomb;
+                }
+
+                if (IsInside(matrixRow, matrixCol - 1) && (matrix[matrixRow, matrixCol - 1] > 0))
+                {
+                    matrix[matrixRow, matrixCol - 1] -= bomb;
+                }
+            }
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                var currentCordinates = coordinates[i].Split(',').Select(int.Parse).ToArray();
+                matrixRow = currentCordinates[0];
+                matrixCol = currentCordinates[1];
+
+                matrix[matrixRow, matrixCol] = 0;
+            }
+
+            int aliveCells = 0;
+            int sumCells = 0;
+
+            foreach (var element in matrix)
+            {
+                if (element > 0)
+                {
+                    aliveCells++;
+                    sumCells += element;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sumCells}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        static void PopulateMatrix()
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+    }
+}
---
+++ [6]26.09-MultidimensionalArrays{EXERCISE]/9. Miner/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9._Miner
+{
+    class Program
+    {
+        static char[,] matrix;
+        static int minorRow;
+        static int minorCol;
+        static int coals;
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            var commands = Console.ReadLine().Split();
+            matrix = new char[fieldSize, fieldSize];
+
+            PopulateMatrix();
+
+            foreach (var currentDirection in commands)
+            {
+                switch (currentDirection)
+                {
+                    case "left":
+                        Move(0, -1);
+                        break;
+                    case "right":
+                        Move(0, 1);
+                        break;
+                    case "up":
+                        Move(-1, 0);
+                        break;
+                    case "down":
+                        Move(1, 0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{coals} coals left. ({minorRow}, {minorCol})");
+        }
+
+        private static void Move(int row, int col)
+        {
+            if (IsInside(minorRow + row, minorCol + col))
+            {
+                minorRow += row;
+                minorCol += col;
+            }
+
+            if (matrix[minorRow, minorCol] == 'e')
+            {
+                Console.WriteLine($"Game over! ({minorRow}, {minorCol})");
+                Environment.Exit(0);
+            }
+
+            if (matrix[minorRow, minorCol] == 'c')
+            {
+                coals--;
+                matrix[minorRow, minorCol] = '*';
+
+                if (coals == 0)
+                {
+                    Console.WriteLine($"You collected all coals! ({minorRow}, {minorCol})");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+
+        private static void PopulateMatrix()
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    char currentChar = char.Parse(input[col]);
+                    matrix[row, col] = currentChar;
+
+                    if (currentChar == 's')
+                    {
+                        minorRow = row;
+                        minorCol = col;
+                    }
+
+                    if (currentChar == 'c')
+                    {
+                        coals++;
+                    }
+                }
+            }
+        }
+    }
+}
